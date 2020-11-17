e63239ee8e1714521f44e981430a2f6699d2fc49
timothee10 <72388955+timothee10@users.noreply.github.com>
2020-11-18 00:06:44 +0100

    s

---
+++ script.js
@@ -0,0 +1,11 @@
+// Create your variables here
+// ==========================================
+
+let numberOfSeasons=10;
+let numberOfEpisodes=6;
+
+// ==========================================
+
+/**let paragraph = document.querySelector('#info');**/
+let paragraph = document.getElementById("info");
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
